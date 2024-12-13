209653feeb1ab773ddfcccc43e7774b92c15e512
DianaDimla <b00150982@mytudublin.ie>
2024-12-13 16:36:52 +0000

    Create Nextconfig.js

---
+++ krispykreme-app/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Set the srcDir to tell Next.js where to find your pages or app directory
+    srcDir: 'src',
+  };
+  
+  module.exports = nextConfig;
+  
