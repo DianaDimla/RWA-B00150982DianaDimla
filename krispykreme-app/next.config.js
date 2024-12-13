ddb1516bf42e8c38bca2eea1b96c18b1773a3b41
DianaDimla <b00150982@mytudublin.ie>
2024-12-13 17:15:10 +0000

    vercel

--- krispykreme-app/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Set the srcDir to tell Next.js where to find your pages or app directory
-    srcDir: 'src',
-  };
-  
-  module.exports = nextConfig;
-  
--- krispykreme-app/next.config.mjs
+++ krispykreme-app/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    srcDir: 'src',
-};
-
-export default nextConfig;
+    experimental: {
+      appDir: true, // Ensure this is set for the App Router
+    },
+    srcDir: 'src', // Tell Next.js to look in 'src' for pages or app
+  };
+  
+  export default nextConfig;
+  
