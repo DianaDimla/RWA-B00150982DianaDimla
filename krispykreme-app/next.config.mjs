81811f635d4c343db61d3c87b44e53a14f58e667
DianaDimla <b00150982@mytudublin.ie>
2024-12-13 18:15:58 +0000

    Update nextConfig

--- krispykreme-app/next.config.mjs
+++ krispykreme-app/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      appDir: true, // Ensure this is set for the App Router
-    },
-    srcDir: 'src', // Tell Next.js to look in 'src' for pages or app
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
